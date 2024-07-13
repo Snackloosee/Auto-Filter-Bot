66a4665298a5fc9eb42477f6952731045444d204
Snackloosee <edosqtf358@gmail.com>
2024-07-13 13:42:20 +0530

    Update channel.py

--- plugins/channel.py
+++ plugins/channel.py
@@ -7,8 +7,29 @@ media_filter = filters.document | filters.video | filters.audio
 
 @Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
 async def media(bot, message):
-    """Media Handler"""
-    media = getattr(message, message.media.value, None)
-    if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
+   for file_type in ("document", "video", "audio"):
+    media = getattr(message, message.media.value, file_type, None)
+       if media is not None:
+           break
+    else:
+        return
+        media.file_type = file_type
         media.caption = message.caption
         await save_file(media)
+    
+       # if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
+
+#from pyrogram import Client, filters, enums
+#from info import INDEX_CHANNELS, INDEX_EXTENSIONS
+#from database.ia_filterdb import save_file
+
+#media_filter = filters.document | filters.video | filters.audio
+
+
+#@Client.on_message(filters.chat(INDEX_CHANNELS) & media_filter)
+#async def media(bot, message):
+#    """Media Handler"""
+#    media = getattr(message, message.media.value, None)
+#    if (str(media.file_name).lower()).endswith(tuple(INDEX_EXTENSIONS)):
+#        media.caption = message.caption
+#        await save_file(media)
